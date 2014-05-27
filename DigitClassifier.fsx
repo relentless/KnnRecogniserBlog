3c3f0af06328d9aec800e8aef5d9d5767967f228
Grant Crofton <grantac@hotmail.com>
2014-05-28 00:06:34 +0100

    Further work, added charting

--- DigitClassifier.fsx
+++ DigitClassifier.fsx
@@ -1,14 +1,18 @@
+#load "packages/FSharp.Charting.0.82/FSharp.Charting.fsx"
+
 open System
 open System.IO
-open Checked
+open FSharp.Charting
 
 (******* 1. GETTING SOME DATA *******)
-let dataLines = File.ReadAllLines(__SOURCE_DIRECTORY__ + """\trainingsample.csv""")
-let dataNoHeader = dataLines.[1..]
+let dataLines = 
+    File.ReadAllLines(__SOURCE_DIRECTORY__ + """\trainingsample.csv""").[1..]
 
 (******* 2. EXTRACTING COLUMNS *******)
-let dataValues = dataNoHeader |> Array.map (fun line -> line.Split(','))
-let dataNumbers = dataValues |> Array.map (Array.map (int)) 
+let dataNumbers = 
+    dataLines 
+    |> Array.map (fun line -> line.Split(','))
+    |> Array.map (Array.map (int)) 
 
 (******* 3. CONVERTING ARRAYS TO RECORDS *******)
 type DigitRecord = { Label:int; Pixels:int[] }
@@ -22,45 +26,40 @@ let trainingSet = dataRecords.[..3999]
 let crossValidationSet = dataRecords.[4000..4499]
 let testSet = dataRecords.[4500..]
 
-(******* 5. COMPUTING DISTANCES *******)
-let distance (testDigit:int[]) (knownDigit:int[]) = 
-    Array.map2 (fun x y -> int64 ((x-y) * (x-y))) testDigit knownDigit
+(******* 5. COMPUTING DISTANCE *******)
+let distanceTo (unknownDigit:int[]) (knownDigit:DigitRecord) = 
+    Array.map2 (
+        fun unknown known -> 
+            let difference = unknown-known
+            int64 (difference * difference)
+        ) unknownDigit knownDigit.Pixels
     |> Array.sum
 
-(******* 9. WRITING THE CLASSIFIER FUNCTION *******)
-
-// use the nearest example
-let classify (unknown:int[]) =
-    let nearestNeighbour = 
-        trainingData
-        |> Array.minBy (fun trainingDigit -> distance trainingDigit.Pixels unknown)
-    
-    nearestNeighbour.Label
-
-// take a vote from the k nearest examples
-let classify_nearest k (unknown:int[]) =
-    trainingData
-    |> Array.sortBy (fun trainingDigit -> distance trainingDigit.Pixels unknown)
+(******* 6. THE CLASSIFIER FUNCTION *******)
+let classifyByNearest k (unknownDigit:int[]) =
+    trainingSet
+    |> Array.sortBy (distanceTo unknownDigit)
     |> Seq.take k
-    |> Seq.groupBy (fun digit -> digit.Label )
-    |> Seq.maxBy (fun (label, digits) -> digits |> Seq.length)
-    |> fst
+    |> Seq.countBy (fun digit -> digit.Label )
+    |> Seq.maxBy (fun (label,count) -> count)
+    |> fun (label,count) -> label
 
-// take all training data into account, but weight score by distance
-let classify_weighted (unknown:int[]) =
-    trainingData
-    |> Seq.groupBy (fun digit -> digit.Label )
-    |> Seq.map (fun (label, group) -> label, group |> Seq.sumBy (fun digit -> 
-        let dist = 1.0 / float (distance digit.Pixels unknown)
-        dist**16.0)) // best from 16 - presumably this is the same as k=1
-    |> Seq.maxBy snd
-    |> fst
+(******* 7. SEE THE CLASSIFIER IN ACTION *******)
+testSet.[..4]
+|> Array.iter (fun digit -> 
+    printfn "Actual: %d, Predicted: %d" 
+        digit.Label 
+        (digit.Pixels |> classifyByNearest 3))
 
-(******* 10. SEE THE CLASSIFIER IN ACTION *******)
-//validationData.[..20]
-//|> Array.iter (fun digit -> printfn "Actual: %d, Predicted: %d" digit.Label (classify digit.Pixels))
+(******* 8. EVALUATING THE MODEL AGAINST VALIDATION DATA *******)
+let calculateAccuracyWithNearest k =
+    testSet
+    |> Array.averageBy (fun digit -> 
+        if digit.Pixels |> classifyByNearest k = digit.Label then 1.0 
+        else 0.0)
 
-(******* 11. EVALUATING THE MODEL AGAINST VALIDATION DATA *******)
-validationData
-|> Array.averageBy (fun digit -> if classify_weighted digit.Pixels = digit.Label then 1.0 else 0.0)
-|> (fun result -> printfn "Correct: %f%%" (result * 100.0))
+let predictionAccuracy = 
+    [1;3;9;27]
+    |> List.map (fun k -> (k, calculateAccuracyWithNearest k))
+
+Chart.Line predictionAccuracy
